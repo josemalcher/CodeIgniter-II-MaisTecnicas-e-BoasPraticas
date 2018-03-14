14c97b67392c4c775408b9a6f8bef088150c31d6
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-03-14 05:47:57 -0300

    07 - Marcando os produtos como vendidos: condicionais e mais migrations - LISTAR APENAS OS NÃO VENDIDOS

--- proj01/application/models/produtos_model.php
+++ proj01/application/models/produtos_model.php
@@ -1,16 +1,22 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class produtos_model extends CI_Model{
+class produtos_model extends CI_Model
+{
 
-    public function buscaTodos(){
+    public function buscaTodos()
+    {
+        $this->db->where("vendido",false);
         return $this->db->get("produtos")->result_array();
     }
 
-    public function salva($produto){
+    public function salva($produto)
+    {
         $this->db->insert("produtos", $produto);
     }
-    public function busca($id){
+
+    public function busca($id)
+    {
         return $this->db->get_where("produtos", array(
             "id" => $id
         ))->row_array();
